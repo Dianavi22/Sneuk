755b7e71c7debe789d245d797bff98bc525861cb
Jado <jade.bouiges@edu.itescia.fr>
2023-03-02 16:37:42 +0100

    add first dialogue, add  save data for menu selector and levelBlocker

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public static DialogueManager instance;
+    private Queue<string> sentences;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de DialogueManager dans la scène");
+            return;
+        }
+
+        instance = this;
+
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+       nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+    void EndDialogue()
+    {
+        print("Fin du dialogue");
+    }
+}
---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public bool isInRange;
+    public Dialogue dialogue;
+    void Update()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.E)) TriggerDialogue();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) isInRange = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (collision.CompareTag("Player")) isInRange = false;
+    }
+
+    void TriggerDialogue()
+    {
+        DialogueManager.instance.StartDialogue(dialogue);
+    }
+}
